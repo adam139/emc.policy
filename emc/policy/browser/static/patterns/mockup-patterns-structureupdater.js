0a7663bf9411dd428721f26446a37d2fae9a7874
adam139 <yuejun.tang@gmail.com>
2020-09-27 17:00:05 +0800

    code clean

--- emc/policy/browser/static/patterns/mockup-patterns-structureupdater.js
+++ emc/policy/browser/static/patterns/mockup-patterns-structureupdater.js
@@ -6,23 +6,19 @@ define([
 ], function(Base) {
     'use strict';
     var Pattern = Base.extend({
-      name: 'structureupdater_emc',  // Give it another name than the original pattern
+      name: 'structureupdater_emc',
       trigger: '.template-folder_contents',
       parser: 'mockup',
       init: function() {
         $('body').on('context-info-loaded', function (e, data) {
-          // Do our task
           if (data.object ) {
-          var $subject = data.object.Subject;
-          
-          
+          var $subject = data.object.Subject;          
             if ($subject.length != 0 && !this.$el.hasClass("tagged"))  {
               var $stag = '<ul id="subject"><li class="first"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span></li>';
               var $etag = '</ul>';
               var out = '';
               for(var j = 0,len = $subject.length; j < len; j++){
                  out = out + '<li><a class ="btn btn-default" href="#">' + $subject[j] + '</a></li>';
-                 //console.log($subject[j]);
                }
                var $tags = $stag + out + $etag;
                $('#plone-document-byline').before(data.object &&  $tags);
